3ecd87aaea2ef4bb58f57870df58b72afcfa679f
Saddock Kabandana <aimegetz@gmail.com>
2024-07-02 12:20:46 +0200

    [#187862829] ft-create-landing-page

--- src/utils/types/store.d.ts
+++ src/utils/types/store.d.ts
@@ -6,3 +6,21 @@ export interface IWelcomeMessage {
 export interface IWelcomeMessageState {
   welcomeMessage: IWelcomeMessage;
 }
+
+export interface IProduct {
+  id: string;
+  shopId: string;
+  name: string;
+  description: string;
+  price: string;
+  discount: string;
+  category: string;
+  expiryDate: string;
+  expired: boolean;
+  bonus: string;
+  images: string[];
+  quantity: string;
+  status: string;
+  createdAt: string;
+  updatedAt: string;
+}

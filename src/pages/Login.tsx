b87abdc97404d720fc816fd9a1510df9a5925c03
AimePazzo <108128511+AimePazzo@users.noreply.github.com>
2024-07-03 17:52:30 +0200

    [Delivers #187584865] started desiging signup page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,6 +4,8 @@ import { Meta } from '../components/Meta'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
 import { BiSolidHide } from "react-icons/bi";
+import { FcGoogle } from "react-icons/fc";
+import { Link } from 'react-router-dom';
 
 const LoginSchema = Yup.object().shape({
   email: Yup.string().email().required('Email is required'),
@@ -24,7 +26,7 @@ export const Login = () => {
 
   return (
     <>
-      <Meta title='Login' />
+      <Meta title='Signup' />
       <div className='home-wrapper'>
         <div className='container'>
           <div className='login-form'>
@@ -32,7 +34,8 @@ export const Login = () => {
               <div className='left-side-login'>
                 <div className='left-side-text'>
                   <h1>
-                    Create e-commerceNinjas account.
+                  Create e-commerceNinjas
+                  account.
                   </h1>
                   <p>
                     Simplify your e-commerce account with our
@@ -46,7 +49,7 @@ export const Login = () => {
               <div className='right-side-login'>
                 <div className='right-side-text'>
                   <h1>Create an account</h1>
-                  <p>Please login to your account</p>
+                  <p>Please create an account to continue</p>
                 </div>
                 <div className='form-box'>
                   <div className='input-box'>
@@ -63,18 +66,30 @@ export const Login = () => {
                   <div className='input-box'>
                     <input
                       type="password"
-                      placeholder='password'
+                      placeholder='Password'
                       id='password'
                       name='password'
                       onChange={formik.handleChange}
                       onBlur={formik.handleBlur}
                       value={formik.values.password} />
                     {formik.touched.password && formik.errors.password ? (<p className='error'>{formik.errors.password}</p>) : null}
-                    <BiSolidHide />
+                    <BiSolidHide className='hide' />
                   </div>
                 </div>
                 <div className='text-left'>
-                  <p>Forgot password</p>
+                  <Link to={'forgot-password'} className='forgot-password'>Forgot password</Link>
+                </div>
+                <div className='btn'>
+                  <button type='submit'>Sign up</button>
+                </div>
+                <div className='line-text'>
+                  <p>or Sign up with</p>
+                </div>
+                <div>
+                  <div className='google'>
+                    <FcGoogle className='google-icon' />
+                    <p>Continue with google</p>
+                  </div>
                 </div>
               </div>
             </form>

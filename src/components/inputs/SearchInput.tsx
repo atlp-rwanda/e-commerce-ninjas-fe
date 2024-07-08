0c151f8eba481a94876df2ce30335e164b3d455f
MANISHIMWESalton <149325279+MANISHIMWESalton@users.noreply.github.com>
2024-07-08 20:43:47 +0200

    [Derivers#18758479] ft-user-search

--- src/components/inputs/SearchInput.tsx
+++ src/components/inputs/SearchInput.tsx
@@ -1,29 +1,56 @@
 /* eslint-disable */
 
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import "../../styles/SearchInput.scss";
+import { useAppDispatch, useAppSelector } from "../../store/store";
 import { useNavigate } from "react-router-dom";
+import {
+  searchProduct,
+  selectSearchResults,
+  selectSearchError,
+} from "../../store/features/ProductSlice";
 interface SearchInputProps {
   className: string;
   placeholder?: string;
 }
 function SearchInput({ className, placeholder }: SearchInputProps) {
-  const [search,setSearch] = useState('')
-const navigate = useNavigate()
+  const dispatch = useAppDispatch();
+  const searchResults = useAppSelector(selectSearchResults);
+  const searchError = useAppSelector(selectSearchError);
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
   return (
-    <>
-    <form className={`search-container ${className}`}>
-      <div className="search-icon">
-        <FiSearch />
-      </div>
-      <input type="text" placeholder={placeholder} onChange={(e)=>setSearch(e.target.value)}/>
-      <button className="search-button" onClick={()=>navigate(`/search?name=${search}`)}>Search</button>
-    </form>
     <div>
+      <form className={`search-container ${className}`}>
+        <div className="search-icon">
+          <FiSearch />
+        </div>
+        <input
+          type="text"
+          placeholder={placeholder}
+          onChange={(e) => {
+            setSearch(e.target.value);
+            dispatch(searchProduct({ name: e.target.value }));
+          }}
+        />
+        <button
+          className="search-button"
+          onClick={() => navigate(`/search?name=${search}`)}
+        >
+          Search
+        </button>
+      </form>
+      <div style={{ display: search ? "block" : "none" }}>
+        {searchResults.length > 0 ? (
+          searchResults.map((product: any) => (
+            <p key={product.id}>{product.name}</p>
+          ))
+        ) : (
+          <p>No product</p>
+        )}
+      </div>
     </div>
-    </>
-
   );
 }
 
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,34 +1,45 @@
 /* eslint-disable */
-import React, { useEffect, useState } from 'react';
-import { useAppDispatch, useAppSelector } from '../store/store';
-import { searchProduct, selectSearchResults, selectSearchError } from '../store/features/ProductSlice';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import {
+  searchProduct,
+  selectSearchResults,
+  selectSearchError,
+} from "../store/features/ProductSlice";
+import { useLocation } from "react-router-dom";
 
 const SearchBar: React.FC = () => {
-    const location = useLocation()
-    const params = new URLSearchParams(location.search);
-    const name = params.get("name")
-    const category = params.get("category")
-    const maxPrice = params.get("maxPrice")
-    const minPrice = params.get("minPrice")
-    
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const name = params.get("name");
+  const category = params.get("category");
+  const maxPrice = params.get("maxPrice");
+  const minPrice = params.get("minPrice");
+
   const dispatch = useAppDispatch();
   const searchResults = useAppSelector(selectSearchResults);
   const searchError = useAppSelector(selectSearchError);
-useEffect(()=>{
-dispatch(searchProduct({name:name?name:undefined,category:category?category:undefined,maxPrice:maxPrice?parseInt(maxPrice):undefined,minPrice:minPrice?parseInt(minPrice):undefined}))
-},[])
+  useEffect(() => {
+    dispatch(
+      searchProduct({
+        name: name ? name : undefined,
+        category: category ? category : undefined,
+        maxPrice: maxPrice ? parseInt(maxPrice) : undefined,
+        minPrice: minPrice ? parseInt(minPrice) : undefined,
+      })
+    );
+  }, []);
   return (
     <div>
-        <h1>
-            search
-        </h1>
+      <h1>search</h1>
       {searchError && <p>Error: {searchError}</p>}
       <div>
         {searchResults.length > 0 ? (
           <ul>
-            {searchResults.map((product:any) => (
-              <li key={product.id}>{product.name} : ${product.price}</li>
+            {searchResults.map((product: any) => (
+              <li key={product.id}>
+                {product.name} : ${product.price}
+              </li>
             ))}
           </ul>
         ) : (

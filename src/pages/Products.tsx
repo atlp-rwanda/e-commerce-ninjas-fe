6beacddb14e346c1eac31a95e9c5c6a8b7b2a7f8
MANISHIMWESalton <149325279+MANISHIMWESalton@users.noreply.github.com>
2024-08-02 13:43:20 +0200

    [Deliver -187584887 & 187584886] A buyer should update delete carts (#57)
    * [Deliver -187584887 & 187584886] A buyer should update delete carts
    
    * fix bugs
    
    * fix bug in deleting cart

---
+++ src/pages/Products.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable */
+import React, { useEffect, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../store/store";
+import { fetchProducts } from "../store/features/product/productSlice";
+import Product from "../components/product/Product";
+import Sample from "../components/layout/Sample";
+import { PuffLoader } from "react-spinners";
+import { Meta } from "../components/Meta";
+import { toast } from "react-toastify";
+import { Navigate, useNavigate } from "react-router-dom";
+import { createCart, getUserCarts } from "../store/features/carts/cartSlice";
+
+const ProductsPage: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate()
+  const [cartResponseData, setCartResponseData] = useState<any>(null)
+  const { products, isError, isSuccess, isLoading, message } = useAppSelector(
+    (state: any) => state.products
+  );
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  const handleAddProductToCart = async (productId: string, quantity = 1) => {
+    try {
+      const response = await dispatch(
+        createCart({ productId, quantity })
+      ).unwrap();
+
+      if (response.data) {
+        toast.success(response.message);
+        const updatedResponse = await dispatch(getUserCarts()).unwrap();
+        setCartResponseData(updatedResponse.data);
+      } else {
+        toast.error(response.message);
+      }
+    } catch (error: any) {
+      if (error === "Not authorized") {
+        localStorage.setItem("pendingCartProduct", productId);
+        toast.error("Please login first");
+        navigate("/login");
+      } else {
+        toast.error("Something went wrong. Please try again later.");
+      }
+    }
+  };
+  useEffect(() => {
+    const checkProductToCartPending = async () => {
+      const pendingProduct = localStorage.getItem("pendingCartProduct");
+      if (pendingProduct) {
+        try {
+          await handleAddProductToCart(pendingProduct, 1);
+          localStorage.removeItem("pendingCartProduct");
+        } catch (error) {
+          console.error("Failed to add product to cart:", error);
+        }
+      }
+    };
+
+    checkProductToCartPending();
+  }, []);
+
+  return (
+    <>
+      <Meta title="Home - E-Commerce Ninjas" />
+      <Sample />
+      <div className="landing-container">
+        {isLoading ? (
+          <div className="loader">
+            <PuffLoader color="#ff6d18" size={300} loading={isLoading} />
+          </div>
+        ) : isError ? (
+          <div className="error-message">
+            <p>{message || "Something went wrong. Please try again later."}</p>
+          </div>
+        ) : (
+          <div>
+            <div className="head">
+              <h1>All products</h1>
+            </div>
+            <div className="product-list">
+              {isSuccess &&
+                Array.isArray(products) &&
+                products?.map((product: any) => (
+                  <Product
+                    key={product.id}
+                    id={product.id}
+                    images={product.images}
+                    name={product.name}
+                    price={`$${product.price}`}
+                    stock={Number(product.quantity)}
+                    description={product.description}
+                    discount={Number(product.discount.replace("%", ""))}
+                  />
+                ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
+};
+
+export default ProductsPage;
--- src/pages/UserViewCart.tsx
+++ src/pages/UserViewCart.tsx
@@ -4,7 +4,7 @@ import { Meta } from "../components/Meta";
 import { useAppDispatch, useAppSelector } from "../store/store";
 import { PuffLoader, PulseLoader } from "react-spinners";
 import { toast } from "react-toastify";
-import { checkout, getUserCarts } from "../store/features/carts/cartSlice";
+import { checkout, getUserCarts , clearCarts,createCart,clearCart,clearCartProduct} from "../store/features/carts/cartSlice";
 import {
   FaCheckSquare,
   FaMinus,
@@ -14,16 +14,25 @@ import {
   FaGift,
   FaShippingFast,
 } from "react-icons/fa";
+import { GiBroom } from "react-icons/gi";
 import { useNavigate } from "react-router-dom";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import Product from "../components/product/Product";
 import { Box, LinearProgress } from "@mui/material";
+import Button from "@mui/material/Button";
+import Dispatch from 'react';
+
 
 const UserViewCart: React.FC = () => {
   const dispatch = useAppDispatch();
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [cartData, setCartData] = useState<any>(null);
-  const [cartResponseData, setCartResponseData] = useState<any>(null);
+  const [cartResponseData, setCartResponseData] = useState(null);
   const [isLoggedOut, setIsLoggedOut] = useState(false);
   const [checkoutData, setcheckoutData] = useState(null);
   const [isCheckoutSuccess, setCheckoutSuccess] = useState(null);
@@ -31,9 +40,15 @@ const UserViewCart: React.FC = () => {
   const [discount, setDiscount] = useState(0);
   const [totalProductPrice, setTotalProductPrice] = useState(0);
   const [arrayOfProduct, setarrayOfProduct] = useState(null);
+  const [quantities, setQuantities] = useState<{ [key: string]: number }>({});
+  const [open,setOpen] = useState(false)
+  
+
   const navigate = useNavigate();
 
   const cartState = useAppSelector((state) => state.cart);
+
+  
 
   useEffect(() => {
     const fetchCarts = async () => {
@@ -86,7 +101,7 @@ const UserViewCart: React.FC = () => {
     );
   }
 
-  if (!cartResponseData || cartResponseData.carts.length < 1) {
+  if (!cartResponseData || cartResponseData?.carts?.length < 1) {
     return (
       <div className="error-message">
         <p>Your cart is empty.</p>
@@ -97,11 +112,11 @@ const UserViewCart: React.FC = () => {
   const handleCartCheckOut = async (cartId, index) => {
     setIsPreloader(true);
     const response = await dispatch(checkout(cartId));
-    const array = cartResponseData.carts[index];
+    const array = cartResponseData?.carts[index];
     setarrayOfProduct(array);
     setcheckoutData(response.payload.data.totalAmount);
     const totalProductPrice = array.products.reduce(
-      (acc, product) => acc + parseFloat(product.price),
+      (acc, product) => acc + parseFloat(product.price) * (product.quantity || 1),
       0
     );
     setTotalProductPrice(totalProductPrice);
@@ -109,6 +124,106 @@ const UserViewCart: React.FC = () => {
     setIsPreloader(false);
     toast.success("Checkout is done Successfully");
   };
+
+  const handleAddProductToCart = async (productId: string, quantity:number) => {
+    try {
+      if (quantity < 1) return; 
+      const response = await dispatch(
+        createCart({ productId, quantity })
+      ).unwrap();
+
+      if (response.data) {
+       
+        setQuantities((prevQuantities) => ({
+          ...prevQuantities,
+          [productId]: quantity,
+        }));
+        toast.success(response.message);
+        const updatedResponse = await dispatch(getUserCarts()).unwrap();
+        setCartResponseData(updatedResponse.data);
+      } else {
+        toast.error(response.message);
+      }
+    } catch (error: any) {
+      if (error === "Not authorized") {
+        localStorage.setItem("pendingCartProduct", productId);
+        toast.error("Please login first");
+        navigate("/login");
+      } else {
+        toast.error("Something went wrong. Please try again later.");
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const incrementQuantity = (productId: string) => {
+    const currentQuantity = quantities[productId] || 1;
+    handleAddProductToCart(productId,currentQuantity+1)
+  };
+
+  const decrementQuantity = (productId: string) => {
+    const currentQuantity = quantities[productId] || 1;
+    if (currentQuantity > 1) {
+      handleAddProductToCart(productId,currentQuantity-1)
+
+    }
+  };
+  const handleClearCart = async () => {
+    try {
+      await dispatch(clearCarts()).unwrap();
+      const response1 = await  dispatch(getUserCarts()).unwrap();
+      setCartResponseData(response1.data);
+      
+      toast.success("Cart cleared successfully");
+      setCartResponseData({ ...cartResponseData, carts: [] });
+      setTotalProductPrice(0);
+    } catch (error) {
+      console.error("Error clearing cart:", error);
+      toast.error("Failed to clear the cart");
+    }
+  };
+
+  const handleClearSingleCart = async (cartId) => {
+  
+    try {
+        await dispatch(clearCart(cartId));
+        const response1 = await  dispatch(getUserCarts()).unwrap();
+        setCheckoutSuccess(false);
+        setCartResponseData(response1.data);
+        const remainingCarts = cartResponseData.carts.filter((cart)=>cart.cartId !== cartId)
+        setCartResponseData({...cartResponseData,carts:remainingCarts});
+        
+    
+        toast.success("Cart cleared successfully");
+    } catch (error) {
+        console.error("Error clearing cart:", error);
+        toast.error("Failed to clear the cart");
+    }
+};
+
+const handleClearCartProduct = async(cartId,productId,cartProductsList)=>{
+  try {
+    if(cartProductsList.length <=1) {
+      await handleClearSingleCart(cartId);
+      return;
+    }
+    const response = await dispatch(clearCartProduct({cartId,productId}))
+    const response1 = await  dispatch(getUserCarts()).unwrap();
+    setCartResponseData(response1.data);
+    toast.success("product cleared successfully");
+  } catch (error) {
+    toast.error("Failed to clear the product ");
+    throw error 
+  }
+}
+
+let cartProductsList=null;
+const handleClose = () => {
+ 
+  setOpen(false);
+};
+
   return (
     <>
       <Meta title="View shopping cart - E-Commerce Ninjas" />
@@ -126,9 +241,15 @@ const UserViewCart: React.FC = () => {
             </Box>
           </div>
         )}
+         <div className="clear-cart">
+          <button className="delete" type="button" onClick={()=>setOpen(true)} >
+            <GiBroom className="deleteIcon" />
+            <p>Delete all Carts</p>
+          </button>
+          </div>
       <section className="cart-section">
         <div className="cart-products">
-          {cartResponseData.carts.map((cart: any, index) => (
+          {cartResponseData?.carts?.map((cart: any, index) => (
             <div key={cart.id} className="cart">
               <div className="title">
                 <FaCheckSquare className="check" color="#ff6d18" />
@@ -139,7 +260,7 @@ const UserViewCart: React.FC = () => {
                   <span>{"Checkout"}</span>
                 </button>
 
-                <FaTrash color="#ff6d18" className="delete" />
+                <FaTrash color="#ff6d18" className="delete" onClick={()=>handleClearSingleCart(cart.cartId)}/>
               </div>
               <div className="products-list">
                 {cart.products.map((product: any) => {
@@ -162,23 +283,19 @@ const UserViewCart: React.FC = () => {
                           </div>
                           <div className="controls">
                             <div className="quantity">
-                              <button className="minus" type="button">
+                              <button className="minus" type="button" onClick={() => decrementQuantity(product.id)}>
                                 <FaMinus />
                               </button>
                               <input
-                                type="number"
                                 value={product.quantity}
                                 readOnly
                               />
-                              <button className="plus" type="button">
+                              <button className="plus" type="button" onClick={() => incrementQuantity(product.id)}>
                                 <FaPlus />
                               </button>
                             </div>
                             <div className="other">
-                              <button className="edit" type="button">
-                                <FaEdit color="#ff6d18" />
-                              </button>
-                              <button className="delete" type="button">
+                              <button className="delete" type="button" onClick={()=>handleClearCartProduct(cart.cartId,product.id,cart.products)}>
                                 <FaTrash color="#ff6d18" />
                               </button>
                             </div>
@@ -201,7 +318,7 @@ const UserViewCart: React.FC = () => {
                   <FaGift />
                 </div>
                 <div className="text">
-                  There are {cartResponseData.carts.length} vouchers for you.
+                  There are {cartResponseData?.carts?.length} vouchers for you.
                 </div>
               </div>
             </div>
@@ -236,6 +353,55 @@ const UserViewCart: React.FC = () => {
               <button>Pay Now</button>
             </div>
           ) : null}
+            <Dialog
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">
+            {"Delete all Carts"}
+          </DialogTitle>
+          <DialogContent>
+            <DialogContentText
+              id="alert-dialog-description"
+              sx={{ fontSize: "1.6rem" }}
+            >
+              Are you sure you want to delete all Carts ?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              onClick={handleClose}
+              sx={{
+                backgroundColor: "primary.main",
+                color: "#fff",
+                fontSize: "1.2rem",
+                "&:hover": {
+                  backgroundColor: "primary.dark",
+                  color: "#fff",
+                },
+              }}
+            >
+              Cancel
+            </Button>
+            <Button
+              onClick={handleClearCart}
+              sx={{
+                backgroundColor: "#ff6d18",
+                color: "#fff",
+                fontSize: "1.2rem",
+                "&:hover": {
+                  backgroundColor: "#e65b00",
+                  color: "#fff",
+                },
+              }}
+              autoFocus
+            >
+              Confirm
+            </Button>
+          </DialogActions>
+        </Dialog>
         </div>
       </section>
     </>
